8d9881a8153d82d1362c46929b0aa3a0bf5b84b8
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2026-03-23 07:33:18 +0100

    bump

--- src/elements/helpers/RenderableDOMElement.ts
+++ src/elements/helpers/RenderableDOMElement.ts
@@ -1,70 +1,66 @@
-/* eslint-disable @typescript-eslint/no-explicit-any -- DOM renderable mixin (proxy prototype) */
-import { extendPrototype, createProxyFunction } from '../../utils/functionExtensions';
+/* eslint-disable @typescript-eslint/no-explicit-any -- DOM renderable: host layer uses mixin `this` */
 import RenderableElement from './RenderableElement';
 
-class RenderableDOMElement {}
+class RenderableDOMElement extends RenderableElement {
+  initElement(this: any, data: any, globalData: any, comp: any) {
+    this.initFrame();
+    this.initBaseData(data, globalData, comp);
+    this.initTransform(data, globalData, comp);
+    this.initHierarchy();
+    this.initRenderable();
+    this.initRendererElement();
+    this.createContainerElements();
+    this.createRenderableComponents();
+    this.createContent();
+    this.hide();
+  }
 
-(function () {
-  const _prototype = {
-    initElement: function (this: any, data: any, globalData: any, comp: any) {
-      this.initFrame();
-      this.initBaseData(data, globalData, comp);
-      this.initTransform(data, globalData, comp);
-      this.initHierarchy();
-      this.initRenderable();
-      this.initRendererElement();
-      this.createContainerElements();
-      this.createRenderableComponents();
-      this.createContent();
-      this.hide();
-    },
-    hide: function (this: any) {
-      // console.log('HIDE', this);
-      if (!this.hidden && (!this.isInRange || this.isTransparent)) {
+  hide(this: any) {
+    if (!this.hidden && (!this.isInRange || this.isTransparent)) {
+      const elem = this.baseElement || this.layerElement;
+      elem.style.display = 'none';
+      this.hidden = true;
+    }
+  }
+
+  show(this: any) {
+    if (this.isInRange && !this.isTransparent) {
+      if (!this.data.hd) {
         const elem = this.baseElement || this.layerElement;
-        elem.style.display = 'none';
-        this.hidden = true;
+        elem.style.display = 'block';
       }
-    },
-    show: function (this: any) {
-      // console.log('SHOW', this);
-      if (this.isInRange && !this.isTransparent) {
-        if (!this.data.hd) {
-          const elem = this.baseElement || this.layerElement;
-          elem.style.display = 'block';
-        }
-        this.hidden = false;
-        this._isFirstFrame = true;
-      }
-    },
-    renderFrame: function (this: any) {
-      // If it is exported as hidden (data.hd === true) no need to render
-      // If it is not visible no need to render
-      if (this.data.hd || this.hidden) {
-        return;
-      }
-      this.renderTransform();
-      this.renderRenderable();
-      this.renderLocalTransform();
-      this.renderElement();
-      this.renderInnerContent();
-      if (this._isFirstFrame) {
-        this._isFirstFrame = false;
-      }
-    },
-    renderInnerContent: function (this: any) {},
-    prepareFrame: function (this: any, num: any) {
-      this._mdf = false;
-      this.prepareRenderableFrame(num);
-      this.prepareProperties(num, this.isInRange);
-      this.checkTransparency();
-    },
-    destroy: function (this: any) {
-      this.innerElem = null;
-      this.destroyBaseElement();
-    },
-  };
-  extendPrototype([RenderableElement, createProxyFunction(_prototype) as any], RenderableDOMElement as any);
-})();
+      this.hidden = false;
+      this._isFirstFrame = true;
+    }
+  }
+
+  renderFrame(this: any) {
+    if (this.data.hd || this.hidden) {
+      return;
+    }
+    this.renderTransform();
+    this.renderRenderable();
+    this.renderLocalTransform();
+    this.renderElement();
+    this.renderInnerContent();
+    if (this._isFirstFrame) {
+      this._isFirstFrame = false;
+    }
+  }
+
+  renderInnerContent(this: any) {}
+
+  prepareFrame(this: any, num: any) {
+    this._mdf = false;
+    this.prepareRenderableFrame(num);
+    this.prepareProperties(num, this.isInRange);
+    this.checkTransparency();
+  }
+
+  destroy(this: any) {
+    this.innerElem = null;
+    this.destroyBaseElement();
+  }
+}
 
 export default RenderableDOMElement;
--- src/utils/functionExtensions.ts
+++ src/utils/functionExtensions.ts
@@ -1,16 +1,39 @@
 type Constructor = { prototype: object };
 
+/** Collect `ctor.prototype` and ancestors up to but not including `Object.prototype`, base-first. */
+function prototypeChainInheritanceOrder(ctor: Constructor): object[] {
+  const out: object[] = [];
+  let p: object | null = ctor.prototype as object | null;
+  while (p && p !== Object.prototype) {
+    out.push(p);
+    p = Object.getPrototypeOf(p) as object | null;
+  }
+  out.reverse();
+  return out;
+}
+
+/**
+ * Copies **own** property descriptors from each source onto `destination.prototype`.
+ * For each source constructor, walks `source.prototype`’s **prototype chain** (excluding
+ * `Object.prototype`), applying **base → derived** within that chain so subclasses flatten
+ * like the old `extendPrototype([Base, Sub])` pair.
+ *
+ * Across `sources`, **later** entries still win on key collision (same as before).
+ */
 function extendPrototype(sources: Constructor[], destination: Constructor): void {
   const destProto = destination.prototype as object;
   const len = sources.length;
   for (let i = 0; i < len; i += 1) {
-    const sourcePrototype = sources[i].prototype as object;
-    const names = Object.getOwnPropertyNames(sourcePrototype);
-    for (let j = 0; j < names.length; j += 1) {
-      const key = names[j];
-      if (key === 'constructor') continue;
-      const desc = Object.getOwnPropertyDescriptor(sourcePrototype, key);
-      if (desc) Object.defineProperty(destProto, key, desc);
+    const chain = prototypeChainInheritanceOrder(sources[i]);
+    for (let c = 0; c < chain.length; c += 1) {
+      const sourcePrototype = chain[c];
+      const names = Object.getOwnPropertyNames(sourcePrototype);
+      for (let j = 0; j < names.length; j += 1) {
+        const key = names[j];
+        if (key === 'constructor') continue;
+        const desc = Object.getOwnPropertyDescriptor(sourcePrototype, key);
+        if (desc) Object.defineProperty(destProto, key, desc);
+      }
     }
   }
 }
@@ -25,4 +48,4 @@ function createProxyFunction(prototype: Record<string, unknown>): new () => Reco
   return ProxyFunction as unknown as new () => Record<string, unknown>;
 }
 
-export { extendPrototype, getDescriptor, createProxyFunction };
+export { extendPrototype, getDescriptor, createProxyFunction, prototypeChainInheritanceOrder };
