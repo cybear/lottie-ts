530574f6d4a28b8019d648dfc1982f8b460d9c2f
Björn Söderqvist <bjorn.soderqvist@ingka.com>
2026-03-23 18:16:56 +0100

    bump

--- src/elements/NullElement.ts
+++ src/elements/NullElement.ts
@@ -1,18 +1,17 @@
-import { extendPrototype } from '../utils/functionExtensions';
 import type { ElementData, GlobalData } from '../types/lottieRuntime';
 import BaseElement from './BaseElement';
 import TransformElement from './helpers/TransformElement';
 import HierarchyElement from './helpers/HierarchyElement';
 import FrameElement from './helpers/FrameElement';
 
-class NullElement {
+class NullElement extends BaseElement {
   declare initFrame: () => void;
-  declare initBaseData: (data: ElementData, globalData: GlobalData, comp: unknown) => void;
   declare initTransform: (data: ElementData, globalData: GlobalData, comp: unknown) => void;
   declare initHierarchy: () => void;
   declare prepareProperties: (num: number, isVisible: boolean) => void;
 
   constructor(data: ElementData, globalData: GlobalData, comp: unknown) {
+    super();
     this.initFrame();
     this.initBaseData(data, globalData, comp);
     this.initFrame();
@@ -38,8 +37,28 @@ class NullElement {
 }
 
 const nullSourceRectAtTime = NullElement.prototype.sourceRectAtTime;
+const getRequiredDescriptor = (proto: object, key: string): PropertyDescriptor => {
+  const desc = Object.getOwnPropertyDescriptor(proto, key);
+  if (!desc) {
+    throw new Error(`Missing descriptor for ${key}`);
+  }
+  return desc;
+};
 
-extendPrototype([BaseElement, TransformElement, HierarchyElement, FrameElement], NullElement);
+Object.defineProperties(NullElement.prototype, {
+  initFrame: getRequiredDescriptor(FrameElement.prototype, 'initFrame'),
+  prepareProperties: getRequiredDescriptor(FrameElement.prototype, 'prepareProperties'),
+  addDynamicProperty: getRequiredDescriptor(FrameElement.prototype, 'addDynamicProperty'),
+  initTransform: getRequiredDescriptor(TransformElement.prototype, 'initTransform'),
+  renderTransform: getRequiredDescriptor(TransformElement.prototype, 'renderTransform'),
+  renderLocalTransform: getRequiredDescriptor(TransformElement.prototype, 'renderLocalTransform'),
+  searchEffectTransforms: getRequiredDescriptor(TransformElement.prototype, 'searchEffectTransforms'),
+  globalToLocal: getRequiredDescriptor(TransformElement.prototype, 'globalToLocal'),
+  initHierarchy: getRequiredDescriptor(HierarchyElement.prototype, 'initHierarchy'),
+  setHierarchy: getRequiredDescriptor(HierarchyElement.prototype, 'setHierarchy'),
+  setAsParent: getRequiredDescriptor(HierarchyElement.prototype, 'setAsParent'),
+  checkParenting: getRequiredDescriptor(HierarchyElement.prototype, 'checkParenting'),
+});
 
 NullElement.prototype.sourceRectAtTime = nullSourceRectAtTime;
 
